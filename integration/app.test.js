d5c5c310b1de74821794c261c20cceb888577488
romish-maker <romish2020@list.ru>
2021-07-16 15:37:03 +0600

    snapshot test for all component

--- integration/app.test.js
+++ integration/app.test.js
@@ -8,3 +8,43 @@ describe('addItemForm', () => {
         expect(image).toMatchImageSnapshot();
     });
 });
+describe('appWithRedux', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=appwithredux--app-with-redux-stories&args=&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+describe('editableSpan', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=editablespan--editable-span-example&args=&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+describe('TaskIsDoneStories', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=task--task-is-done-stories&args=&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+describe('TaskIsNotDoneStories', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=task--task-not-done-stories&args=&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
